0fde3568ee3fff9690f9d0b9edf0fee97daaba08
FlavioDev <fdqflaviodelacruz@gmail.com>
2025-05-26 14:02:58 -0500

    2.0

--- js/pulso.js
+++ js/pulso.js
@@ -1,11 +1,10 @@
+const redes = document.querySelector('.redes');
+
 observer.observe(redes);
 
-  const redes = document.querySelector('.redes');
-
-  redes.addEventListener('mouseenter', () => {
-    redes.classList.add('hovered');
-    setTimeout(() => {
-      redes.classList.remove('hovered');
-    }, 2000);
-  });
-  
+redes.addEventListener('mouseenter', () => {
+  redes.classList.add('hovered');
+  setTimeout(() => {
+    redes.classList.remove('hovered');
+  }, 2000);
+});
