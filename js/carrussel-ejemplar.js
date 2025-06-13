a7a7342949df7b7e270a2b0f015b22cce661b623
FlavioDev <fdqflaviodelacruz@gmail.com>
2025-06-13 01:55:00 -0500

    mejoras en html css y js

--- js/carrussel-ejemplar.js
+++
@@ -1,5 +0,0 @@
-  document.addEventListener('DOMContentLoaded', () => {
-    const carrusel = document.getElementById('carrusel');
-    const clones = carrusel.cloneNode(true);
-    carrusel.append(...clones.children);
-  });
--- js/progress.js
+++
@@ -1,9 +0,0 @@
-  window.addEventListener('load', () => {
-    const bar = document.getElementById('progress-bar');
-    bar.style.width = '100%';
-
-    // Espera un momento y lo oculta
-    setTimeout(() => {
-      document.getElementById('progress-bar-container').style.display = 'none';
-    }, 600); // puedes ajustar el tiempo
-  });
